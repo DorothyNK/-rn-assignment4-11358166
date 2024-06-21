2f013b05d6055e65698d50d885ebdd1d72f0b995
unknown <dorothykotey66@gmail.com>
2024-06-20 19:48:41 -0700

    message

--- my-expoapp1/App.js
+++ my-expoapp1/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import LoginScreen from './LoginScreen';
+import Home from './HomeScreen';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <LoginScreen />
+      <Home/>
     </View>
   );
 };
